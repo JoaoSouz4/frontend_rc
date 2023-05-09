20a14c5daf25d09fce2deaaa7b8722b72ccaf019
João Souza <joaosouz8@gmail.com>
2023-05-08 21:39:06 -0300

    correção no componente AnotherComment

--- src/pages/Ilustracoes/Commentary/AnotherComment/index.jsx
+++ src/pages/Ilustracoes/Commentary/AnotherComment/index.jsx
@@ -3,10 +3,15 @@ import styles from './AnotherComment.module.css';
 
 export function AnotherComment (props) {
     const { comment, userName, createdAt } = props;
+    const date = new Date(createdAt);
+    const day = date.getDate();
+    const mes = date.getMonth();
+    const ano = date.getFullYear();
+    const dateComment = `${day}/${mes+1}/${ano}`
     return (
         <Wrapper flexDirection = 'column' jc = 'flex-start' width = '100%' >
             <p className = {styles.comment}><strong>{userName}</strong> {comment}</p>
-            <span className = {styles.dateComment}>{createdAt} </span>
+            <span className = {styles.dateComment}>{dateComment} </span>
         </Wrapper>
     )
 }
